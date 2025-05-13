3f8cadecc7bdeac634a83309ab9c3b50be42de70
HAMZA-CH <hakram554r@gmail.com>
2025-05-13 17:26:34 +0500

    minor changes

--- lib/Features/SplashScreen/splash_screen.dart
+++ lib/Features/SplashScreen/splash_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:just_talk/Features/LoginScreen/login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    var navigate = Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ));
+    Future.delayed(Duration(seconds: 3), () {
+      navigate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
