d312a2aebf9bd2650b37f2be9d9f728fc72acd97
HAMZA-CH <hakram554r@gmail.com>
2025-05-21 00:03:40 +0500

    minor changes

--- lib/Features/ProfileScreen/profile_screen.dart
+++ lib/Features/ProfileScreen/profile_screen.dart
@@ -1,10 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:just_talk/Features/ProfileScreen/Widgets/information_tile.dart';
 import 'package:just_talk/UiHelpers/Utils/Color_Palette/color_palette.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key, required this.userId});
+  final String userId;
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore fireStore = FirebaseFirestore.instance;
+  late Map<String, dynamic> userMap;
+  late Future<void> _fetchUsersFuture;
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsersFuture = fetchUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,46 +31,99 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Profile"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          spacing: 18,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: CircleAvatar(
-                radius: 85,
-                backgroundColor: Palette.primaryColor,
-                child: Icon(
-                  CupertinoIcons.person,
-                  color: Colors.white,
-                  size: 50,
-                ),
+      body: FutureBuilder(
+        future: _fetchUsersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Unknown Error"));
+          } else {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                spacing: 18,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: CircleAvatar(
+                      radius: 85,
+                      backgroundColor: Palette.primaryColor,
+                      child: Icon(
+                        CupertinoIcons.person,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      InformationTile(
+                        title: "Name",
+                        subtitle: userMap[widget.userId]["name"],
+                        icon: CupertinoIcons.person,
+                        onTap: () {},
+                        isLoggedUser: userMap[widget.userId]["name"] ==
+                            auth.currentUser!.displayName,
+                      ),
+                      InformationTile(
+                        title: "About",
+                        subtitle: userMap[widget.userId]["about"],
+                        icon: Icons.info_outline,
+                        onTap: () {},
+                        isLoggedUser: userMap[widget.userId]["name"] ==
+                            auth.currentUser!.displayName,
+                      ),
+                      InformationTile(
+                        title: "Email Address",
+                        subtitle: userMap[widget.userId]["email"],
+                        icon: Icons.email_outlined,
+                        onTap: () {},
+                        isLoggedUser: userMap[widget.userId]["name"] ==
+                            auth.currentUser!.displayName,
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      await auth.signOut();
+                      Navigator.pop(context);
+                    },
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        Text(
+                          "LogOut",
+                          style: GoogleFonts.publicSans(
+                              color: Colors.red, fontSize: 18),
+                        ),
+                        Icon(
+                          Iconsax.logout,
+                          color: Colors.red,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Column(
-              children: [
-                InformationTile(
-                  title: "Name",
-                  subtitle: "Hamza Ch",
-                  icon: CupertinoIcons.person,
-                ),
-                InformationTile(
-                  title: "About",
-                  subtitle: "Hi! im' using just talk",
-                  icon: Icons.info_outline,
-                ),
-                InformationTile(
-                  title: "Email Address",
-                  subtitle: "hamza@gmail.com",
-                  icon: Icons.email_outlined,
-                ),
-              ],
-            ),
-          ],
-        ),
+            );
+          }
+        },
       ),
     );
+  }
+
+  /// Method to fetch all users from DB
+  Future fetchUsers() async {
+    try {
+      QuerySnapshot querySnapshot = await fireStore.collection("users").get();
+
+      userMap = Map.fromEntries(querySnapshot.docs.map(
+        (doc) => MapEntry(doc.id, doc.data()),
+      ));
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
