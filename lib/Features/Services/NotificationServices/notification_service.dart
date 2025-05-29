040c29b8d77736443d5b1b6c201c0e6d1d0ed548
HAMZA-CH <hakram554r@gmail.com>
2025-05-29 20:50:11 +0500

    minor changes

---
+++ lib/Features/Services/NotificationServices/notification_service.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+
+class NotificationServices {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  Future<void> notificationSettings() async {
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      debugPrint("Permission Granted by user");
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      debugPrint("User granted provisional permission");
+    } else {
+      debugPrint("Permission denied by user");
+    }
+  }
+}
