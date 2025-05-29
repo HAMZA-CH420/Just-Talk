d0007e899bf16d2372a7bd743abf121b87e24236
HAMZA-CH <hakram554r@gmail.com>
2025-05-29 21:00:18 +0500

    minor changes

--- lib/Features/Services/NotificationServices/notification_service.dart
+++ lib/Features/Services/NotificationServices/notification_service.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
 class NotificationServices {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
   Future<void> notificationSettings() async {
     NotificationSettings settings = await _firebaseMessaging.requestPermission(
       alert: true,
